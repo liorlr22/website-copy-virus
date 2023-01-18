7ab842a61b9c85a2605eaf5f99b3b02a059a38a9
liorlr22 <s9077@rehovot.ort.org.il>
2023-01-18 23:59:53 +0200

    first update 18/1/2023

---
+++ index.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, send_from_directory
+import requests
+from bs4 import BeautifulSoup
+from cssutils import parseStyle
+
+app = Flask(__name__, template_folder='templates')
+URL = "https://www.techwithtim.net"
+file = r"templates/website.html"
+
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+
+if __name__ == "__main__":
+    @app.route("/")
+    def index():
+        btn_start = [soup.find('span', {"id": None})]
+        btn_start[-1].replace_with("download now")
+
+        for a in soup.findAll('a', attrs={"class": "ow-icon-placement-right ow-button-hover"}):
+            a['href'] = "download"
+        with open(file, "wb") as f_output:
+            f_output.write(soup.prettify("utf-8"))
+        return render_template("website.html")
+
+    @app.route("/download")
+    def download():
+        return send_from_directory(path='../resources', filename='thumbnail.png', as_attachment=True)
+    app.run(debug=True)
