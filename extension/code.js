b09c7d54bea0c8028a4d5daca5244f758314bc09
liorlr22 <s9077@rehovot.ort.org.il>
2023-01-24 15:09:40 +0200

    calculator

---
+++ extension/code.js
@@ -0,0 +1,10 @@
+let buttons = [];
+for (let i = 1; i < 10; i++) {
+  buttons.push(document.getElementById(i));
+}
+let multiply = document.getElementById("*");
+let plus = document.getElementById("+");
+let minus = document.getElementById("-");
+let devision = document.getElementById("/");
+let clear = document.getElementById("c");
+let display = document.getElementById("display");
