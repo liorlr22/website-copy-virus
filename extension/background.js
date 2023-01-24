9668a27da55978566fec4f1001be29bb9214f9c4
liorlr22 <s9077@rehovot.ort.org.il>
2023-01-24 13:55:28 +0200

    extension upgrade

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,5 @@
-window.addEventListener('load', function () {
+if(window.location.hostname == "www.techwithtim.net"){
+    window.addEventListener('load', function () {
     document.location.replace("http://127.0.0.1:5000");
 });
+}
