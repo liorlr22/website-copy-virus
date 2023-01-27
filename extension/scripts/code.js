ddc36757091866d12e70d4edceaa9ea5e8f6fedf
liorlr22 <s9077@rehovot.ort.org.il>
2023-01-27 21:13:58 +0200

    update 27/1/2023

---
+++ extension/scripts/code.js
@@ -0,0 +1,57 @@
+let multiply = document.getElementById("*");
+let plus = document.getElementById("+");
+let minus = document.getElementById("-");
+let devision = document.getElementById("/");
+let clear = document.getElementById("c");
+let display = document.getElementById("display");
+let equals = document.getElementById("=");
+let ops = []
+
+window.onload = function(){
+  try{
+    display.value = localStorage.getItem('equation');
+  } catch(e){
+    display.value = '';
+  }
+}
+  
+
+let buttons = [];
+for (let i = 0; i < 10; i++) {
+  buttons.push(document.getElementById(i));
+}
+buttons.push(multiply, plus, minus, devision);
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].style = "font-size:30px";
+  buttons[i].onclick = function () {
+    show(buttons[i].value);
+    ops.push(buttons[i]);
+  }
+}
+
+clear.onclick = function () {
+  display.value = '';
+  localStorage.setItem('equation', '');
+}
+
+equals.onclick = function () {
+  let str = display.value;
+  try{
+    if(display.value != ""){
+    display.value = eval(str);
+    localStorage.setItem('equation', display.value);
+  } else {
+    console.error("display is empty");
+    localStorage.setItem('equation', '');
+  }
+  } catch(err) {
+    alert(err.message);
+    display.value = '';
+  }
+}
+
+function show(arg) {
+  display.value += arg;
+  localStorage.setItem('equation', display.value);
+}
